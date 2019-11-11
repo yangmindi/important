b6e18c03193d49ec47080e8c5f00e964c26ed890
ymd <1946334058@qq.com>
2019-11-11 23:18:48 +0800

    seeOkByVolatile

--- src/com/ymd/face/VolatileDemo.java
+++ src/com/ymd/face/VolatileDemo.java
@@ -1,7 +1,9 @@
 package com.ymd.face;
 
+import java.util.concurrent.TimeUnit;
+
 class MyData {
-    int number = 0;
+    volatile int number = 0;
 
     public void addTo60(){
         this.number = 60;
@@ -9,5 +11,29 @@ class MyData {
 }
 
 public class VolatileDemo{
+    public static void main(String[] args) {
 
+    }
+
+    public static void seeOkByVolatile(){
+        MyData myData = new MyData();//资源类
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName()+"\t come in");
+            //暂停一会线程
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            myData.addTo60();
+            System.out.println(Thread.currentThread().getName()+"\t update number value:"+myData.number);
+        },"AAA").start();
+
+        //第二个线程就是我们的主线程
+        while(myData.number == 0){
+            //mian线程就一直在这里循环，知道number值不再等于零
+        }
+        System.out.println(Thread.currentThread().getName()+"\t mission is over,main get number value:"+myData.number);
+    }
 }
