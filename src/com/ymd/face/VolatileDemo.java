ac3679a8d4ae8d1982681c2791213367e9299e9d
ymd <1946334058@qq.com>
2019-11-11 23:28:22 +0800

    volatile AtomicInteger

--- src/com/ymd/face/VolatileDemo.java
+++ src/com/ymd/face/VolatileDemo.java
@@ -1,18 +1,46 @@
 package com.ymd.face;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class MyData {
     volatile int number = 0;
+    AtomicInteger atomicInteger = new AtomicInteger();
 
     public void addTo60(){
         this.number = 60;
+    }
+
+    //请注意，此时number前面是加了volatile关键字修饰的，volatile不能保证原子性
+    public void addPlusPlus(){
+        number++;
+    }
+
+    public void addMyAtomic(){
+        atomicInteger.getAndIncrement();
     }
 }
 
 public class VolatileDemo{
     public static void main(String[] args) {
+        MyData myData = new MyData();
 
+        for(int i=1; i<=20; i++){
+            new Thread(()->{
+                for(int j=1; j<=1000; j++){
+                    myData.addPlusPlus();
+                    myData.addMyAtomic();
+                }
+            },String .valueOf(i)).start();
+        }
+
+        //需要等待上面20个线程全部计算完成之后，再用main线程取得最终的结果值是多少？
+        while(Thread.activeCount() > 2){
+            Thread.yield();
+        }
+
+        System.out.println(Thread.currentThread().getName()+"\t int type,finally number value:"+myData.number);
+        System.out.println(Thread.currentThread().getName()+"\t AtomicInteger type,finally number value:"+myData.atomicInteger);
     }
 
     public static void seeOkByVolatile(){
