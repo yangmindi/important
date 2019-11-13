0542f41e76d87d9edbf871b9fa6739a95054bb1b
ymd <1946334058@qq.com>
2019-11-13 21:39:19 +0800

    cas

---
+++ src/com/ymd/face/CAS/CASDemo.java
@@ -0,0 +1,13 @@
+package com.ymd.face.CAS;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CASDemo {
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+
+        //main do thing...
+        System.out.println(atomicInteger.compareAndSet(5,2019)+"\t current data:"+atomicInteger.get());
+        System.out.println(atomicInteger.compareAndSet(5,1024)+"\t current data:"+atomicInteger.get());
+    }
+}
